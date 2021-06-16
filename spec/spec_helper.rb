f2f75c139da9c69cfe19603598f346b0cbace4de
Craig Eaton <craigea92@gmail.com>
2021-06-16 17:00:47 +0100

    Updated bookmark_manager - Added Testing Environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,10 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+require_relative './setup_test_database'
 
 # Set the environment to "test"
-ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -28,6 +29,10 @@ Capybara.app = BookmarkManager
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
