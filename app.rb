74661ca9307999c4f5b7e4b53b6a547620113331
Craig Eaton <craigea92@gmail.com>
2021-06-17 16:47:58 +0100

    Bookmarks are wrapped in bookmark objects, and users can now add and view bookmark titles

--- app.rb
+++ app.rb
@@ -12,10 +12,17 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
-    # p ENV
-
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
